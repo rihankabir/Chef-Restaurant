d3781673fe2492ee71c0b6a1fe983f457a1decca
Md Rihanul kabir <rihankabir006@gmail.com>
2025-05-08 18:16:33 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//alert("bangladesh");
+/*var x = 4;
+x += 4;
+console.log(x)
+*/
+
+/*var x = 7;
+x -= 6;
+console.log(x)
+*/
+const userName = "Rihanul Kabir";
+const income = 6000;
+let rent = 1300;
+let groceries = 2000;
+let transport = 400;
+ 
+ let totalExpences = rent + groceries + transport;
+ console.log(`My name is: ${userName}`);
+ console.log(totalExpences);
+
+ /*var num1 = 50;
+ var num2 = 70;
+ var num3 = 80;
+
+ console.log(num1 >= num2);
+ console.log(num1>num2 && num1 > num3);
+console.log(num1>num2 || num3>num2)
+//
+var Number = prompt("Enter your number:");
+
+if (Number > 80) {
+	console.log("A+")
+}else if (Number < 70-80) {
+	console.log("A")
+}else if(Number < (60-70){
+	console.log("A-")
+}else if(Number < (50-60){
+	console.log("B")
+}else if (Number < (40-50) {
+	console.log("C")
+}else if(Number < (33-40){
+	console.log("D")
+}else if(Number < 30){
+	console.log("F")
+}*/
+ var letter = prompt("Enter your Letter :");
+ if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
+ 	console.log("vowel")
+ else
+ 	console.log("consunant");
