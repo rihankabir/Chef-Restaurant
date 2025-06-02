7b33b49f8bc17b8b15f41fb905c81a8ab7bf664f
Md Rihanul kabir <rihankabir006@gmail.com>
2025-06-02 22:55:26 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,15 +8,15 @@ console.log(x)
 x -= 6;
 console.log(x)
 */
-const userName = "Rihanul Kabir";
-const income = 6000;
-let rent = 1300;
-let groceries = 2000;
-let transport = 400;
+//const userName = "Rihanul Kabir";
+//const income = 6000;
+//let rent = 1300;
+//let groceries = 2000;
+//let transport = 400;
  
- let totalExpences = rent + groceries + transport;
- console.log(`My name is: ${userName}`);
- console.log(totalExpences);
+ //let totalExpences = rent + groceries + transport;
+ //console.log(`My name is: ${userName}`);
+ //console.log(totalExpences);
 
  /*var num1 = 50;
  var num2 = 70;
@@ -43,8 +43,8 @@ if (Number > 80) {
 }else if(Number < 30){
 	console.log("F")
 }*/
- var letter = prompt("Enter your Letter :");
- if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
- 	console.log("vowel")
- else
- 	console.log("consunant");
+ //var letter = prompt("Enter your Letter :");
+ //if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
+// 	console.log("vowel")
+ //else
+ 	//console.log("consunant");
